e7b493c13d894f4792be973d33aee5d049f85966
Ivan <damnedelf@ya.ru>
2021-02-04 12:21:20 +0300

    second try

--- ap.js
+++ ap.js
@@ -1,3 +1,4 @@
+//buttons ==> got caught from DOM
 let cartmanBtn = document.querySelector('#cartman');
 let kennyBtn = document.querySelector('#kenny');
 let stanBtn = document.querySelector('#stan');
@@ -5,23 +6,30 @@ let kayleBtn = document.querySelector('#kayle');
 let buttersBtn = document.querySelector('#butters');
 let imgToChange = document.querySelector(`#img-to-change`);
 let btnToChange= document.querySelector("#name");
-cartmanBtn.addEventListener('click',function(){
-   btnToChange.innerHTML = 'KILL CARTMAN';
-   imgToChange.setAttribute('src','img/cartman.gif')
-})
-kennyBtn.addEventListener('click',function(){
-    btnToChange.innerHTML = 'KILL KENNY';
-    imgToChange.setAttribute('src','img/kenny.jpg')
- })
- stanBtn.addEventListener('click',function(){
-    btnToChange.innerHTML = 'KILL STAN';
-    imgToChange.setAttribute('src','img/stan.png')
- })
- kayleBtn.addEventListener('click',function(){
-    btnToChange.innerHTML = 'KILL KAYLE';
-    imgToChange.setAttribute('src','img/Kyle-broflovski.png')
- })
- buttersBtn.addEventListener('click',function(){
-    btnToChange.innerHTML = 'KILL BUTTERS';
-    imgToChange.setAttribute('src','img/ButtersStotch.png')
- })
+let textToChange = document.querySelector('.description')
+
+//button on click handler
+let menuClickHandler = function(button, buttonName, pathToImage, pathToText){
+    button.addEventListener("click",function(){
+        btnToChange.innerHTML = buttonName;
+        imgToChange.setAttribute(`src`, pathToImage);
+      
+    setTimeout(()=>  imgToChange.setAttribute(`src`, 'img/enemy_uknown.jpg') ,1000);
+    setTimeout(()=>imgToChange.setAttribute('src', pathToImage),2000);
+    fetch(pathToText)
+    .then(response => response.text())
+    .then(text => textToChange.innerHTML=text)
+    });
+     
+ 
+    
+
+}
+//execute handlers
+menuClickHandler(cartmanBtn, 'KILL Cartman','img/cartman.gif' ,`text/cartman.txt`);
+menuClickHandler(kennyBtn, 'KILL Kenny','img/kenny.jpg',`text/kenny.txt` );
+menuClickHandler(stanBtn, 'KILL Stan','img/stan.png',`text/stan.txt` );
+menuClickHandler(kayleBtn, 'KILL Kayle','img/Kyle-broflovski.png',`text/kyle.txt` );
+menuClickHandler(buttersBtn, 'KILL Butters','img/ButtersStotch.png' ,`text/butters.txt`);
+
+
